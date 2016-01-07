bbe638d3d356f14b24d2462903407bbd47919bcf
John6tain4eto <john6tain.tube@gmail.com>
2016-01-07 07:01:33 +0200

    The sick wheel is here
    this shit is now working properly

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,3 +1,19 @@
 function Load(){
     window.open("main.html");
 }
+
+	var deg = Math.floor((Math.random() * 1000) + 100);
+    $("#start").on('click', function() {
+        $('#wheel').css({
+			'-webkit-transform': 'rotate('+100+'deg)',
+			/*'-moz-transform': 'rotate('+deg+'deg)',
+			'-ms-transform': 'rotate('+deg+'deg)',
+			'-o-transform': 'rotate('+deg+'deg)',
+			'transform': 'rotate('+deg+'deg)'*/
+			
+        });
+        deg-=40;
+		
+		
+    });
+	
