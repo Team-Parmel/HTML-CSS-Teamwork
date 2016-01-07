5e00d7fa414b282a734c023a0e0bcb90aab2ddeb
npaunov <nvpaunov@gmail.com>
2016-01-07 10:20:41 +0200

    Final!

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,19 +1,3 @@
 function Load(){
     window.open("main.html");
 }
-
-	var deg = Math.floor((Math.random() * 1000) + 100);
-    $("#start").on('click', function() {
-        $('#wheel').css({
-			'-webkit-transform': 'rotate('+100+'deg)',
-			/*'-moz-transform': 'rotate('+deg+'deg)',
-			'-ms-transform': 'rotate('+deg+'deg)',
-			'-o-transform': 'rotate('+deg+'deg)',
-			'transform': 'rotate('+deg+'deg)'*/
-			
-        });
-        deg-=40;
-		
-		
-    });
-	
